8e525782214212733be400c393d54425b0d7911f
exister <exister@tutanota.com>
2022-11-10 16:12:00 -0500

    adjust my script to remove Routes before protoc.

--- Sample Code/app/src/main/java/com/dji/sdk/sample/tigersalvage/proto/schemas/protoc.sh
+++ Sample Code/app/src/main/java/com/dji/sdk/sample/tigersalvage/proto/schemas/protoc.sh
@@ -1,1 +1,2 @@
+rm /home/exister/Documents/school_docs/Senior\ Project/android-bundle/Sample\ Code/app/src/main/java/com/dji/sdk/sample/tigersalvage/proto/schemas/generated/Route*
 protoc -I=/home/exister/Documents/school_docs/Senior\ Project/android-bundle/Sample\ Code/app/src/main/java/com/dji/sdk/sample/tigersalvage/proto/schemas --java_out=/home/exister/Documents/school_docs/Senior\ Project/android-bundle/Sample\ Code/app/src/main/java/ Route.proto 
