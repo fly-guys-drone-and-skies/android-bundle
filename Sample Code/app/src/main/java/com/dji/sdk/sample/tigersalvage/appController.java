582803a24d1117446d93d65c02daa9e43307e186
exister <exister@tutanota.com>
2022-11-10 12:51:12 -0500

    minor refactoring and cleaning up

--- Sample Code/app/src/main/java/com/dji/sdk/sample/tigersalvage/appController.java
+++ Sample Code/app/src/main/java/com/dji/sdk/sample/tigersalvage/appController.java
@@ -1,17 +1,27 @@
 package com.dji.sdk.sample.tigersalvage;
+
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+
+import dji.sdk.sdkmanager.DJISDKManager;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+import com.dji.sdk.sample.tigersalvage.Sender;
+
 public class appController {
 
     public appController(){
+        initMessaging();
+        initSDK();
+    }
+
+    private void initSDK() {
 
     }
 
-    void init() throws IOException, TimeoutException {
+    void initMessaging() throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         Connection connection = factory.newConnection();
 
