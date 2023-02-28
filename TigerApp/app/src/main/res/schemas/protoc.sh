ee42da682369cf83cf811fec74c3bc5f9be8ebf2
Joseph Maldonado <jam1953@rit.edu>
2023-02-28 01:26:04 -0500

    modify script for proper directories.

--- TigerApp/app/src/main/res/schemas/protoc.sh
+++ TigerApp/app/src/main/res/schemas/protoc.sh
@@ -1,10 +1,11 @@
 # Run this from this directory.
-rm generated/Route*
+rm -r ../../java/src/main/com/dji/TigerApp/protobuf/
+
 # Assumes you have the protobuf-schemas repo right next to the this (the android-bundle) repo.
 # Should just use the ones in this directory already if you don't.
 cp ../../../../../../../protobuf-schemas/*.proto ./
 sed -i "s/PACKAGENAME/com.dji.TigerApp.protobuf/g" VehicleStatus.proto
 sed -i "s/PACKAGENAME/com.dji.TigerApp.protobuf/g" Route.proto
 
-protoc -I=./ --java_out=../java/ Route.proto
-protoc -I=./ --java_out=../java VehicleStatus.proto
+protoc -I=./ --java_out=../../java/ Route.proto
+protoc -I=./ --java_out=../../java VehicleStatus.proto
