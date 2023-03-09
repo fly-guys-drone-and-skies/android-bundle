8e11585da83f385d884cfeb390a56ceecb6089a0
Joseph Maldonado <josephamaldonadoii@gmail.com>
2023-03-09 13:08:03 -0500

    add incomplete operatorlistener.

---
+++ TigerApp/app/src/main/java/com/dji/TigerApp/OperatorListener.java
@@ -0,0 +1,74 @@
+package com.dji.TigerApp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+
+import com.dji.TigerApp.Mission.WaypointMissionList;
+
+import dji.common.error.DJIError;
+
+import dji.common.flightcontroller.FlightControllerState;
+import dji.common.mission.waypoint.WaypointMissionDownloadEvent;
+import dji.common.mission.waypoint.WaypointMissionExecutionEvent;
+import dji.common.mission.waypoint.WaypointMissionUploadEvent;
+import dji.common.util.CommonCallbacks;
+import dji.sdk.mission.waypoint.WaypointMissionOperator;
+import dji.sdk.mission.waypoint.WaypointMissionOperatorListener;
+
+public class OperatorListener implements WaypointMissionOperatorListener {
+    private WaypointMissionList missionList;
+    private FlightControllerState flightControllerState;
+    private WaypointMissionOperator operator;
+
+    public OperatorListener(WaypointMissionList missionList, FlightControllerState flightControllerState) {
+        this.missionList = missionList;
+        this.flightControllerState = flightControllerState;
+        this.operator = operator;
+    }
+
+    public void onExecutionFinish(DJIError error) {
+        if (error == null) {
+            //operator.startMission(completionCallback);
+        }
+    }
+
+    public void onDownloadUpdate(@NonNull WaypointMissionDownloadEvent waypointMissionDownloadEvent) {
+        return;
+    }
+
+    public void onUploadUpdate(@NonNull WaypointMissionUploadEvent waypointMissionUploadEvent) {
+        return;
+    }
+
+    public void onExecutionUpdate(@NonNull WaypointMissionExecutionEvent waypointMissionExecutionEvent) {
+        // Status status = new Status(
+        //     flightControllerState.getAircraftLocation(),
+        //     flightControllerState.getAttitude(),
+        //     new float[] {
+        //             flightControllerState.getVelocityX(),
+        //             flightControllerState.getVelocityY(),
+        //             flightControllerState.getVelocityZ(),
+        //     },
+        //     operator.getCurrentState().toString()
+        // );
+
+        //Sender.send(status.toMessage().toByteArray(), "ui-exchange", "status", "ui");
+    }
+
+    public void onExecutionStart() {
+        if(missionList.isComplete()) {
+            return;
+        }
+        operator.uploadMission(
+            (DJIError uploadError) -> {
+                if (uploadError != null) { // TODO Make this generic?
+                    System.out.println(uploadError.getDescription());
+                }
+                else {
+                    missionList.loadNextMission(operator);
+                }
+            }
+        );
+    }
+}
